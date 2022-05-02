ad3b5557d2401e44cff12cee3732e65e6959cf37
katia-garcia <katia.gaag@gmail.com>
2022-05-02 23:57:14 +0200

    vista menuprincipal
    creados los componentes principales. y su estructura basica.

--- frontend/src/components/MenuPrincipal.vue
+++ frontend/src/components/MenuPrincipal.vue
@@ -1,6 +1,60 @@
 <template>
-  <NavBar></NavBar>
+  <div>
+    <NavBar></NavBar>
+    <b-container fluid>
+      <b-row style="height: 100%">
+        <!-- sidebar -->
+        <b-col cols="3" style="background-color:#f2f2f2; padding-top: 15px; padding-left: 20px; padding-right: 20px">
+          <b-row>
+            <b-link href="#qualificacions" class="custom-sidebar-text">
+              <b-icon-clipboard-check></b-icon-clipboard-check>&nbsp;&nbsp;&nbsp;Qualificacions
+            </b-link>
+          </b-row>
+          <b-row style="margin-top: 10px">
+            <b-link class="custom-sidebar-text">
+              <b-icon-megaphone></b-icon-megaphone>&nbsp;&nbsp;&nbsp;Fòrum de la Facultat
+            </b-link>
+          </b-row>
+          <b-row style="margin-top: 10px">
+            <b-link class="custom-sidebar-text">
+              <b-icon-chat-dots></b-icon-chat-dots>&nbsp;&nbsp;&nbsp;Xat
+            </b-link>
+          </b-row>
+          <b-row style="margin-top: 10px">
+            <b-link class="custom-sidebar-text">
+              <b-icon-calendar-event></b-icon-calendar-event>&nbsp;&nbsp;&nbsp;Calendari
+            </b-link>
+          </b-row>
+          <b-calendar style="margin-top: 20px; " block
+            nav-button-variant="primary"
+          ></b-calendar>
+        </b-col> <!-- END sidebar -->
+        <b-col>
+          <div class="row">
+            <div class="col-lg-4 col-md-6 mb-4" v-for="(asignatura) in asignaturas" :key="asignatura">
+              <b-card class="custom-card-asignatura" header="danger">
+                <template #header style="align-items: center;">
+                  <b-avatar></b-avatar>
+                  <b-avatar></b-avatar>
+                  <b-avatar></b-avatar>
+                </template>
+                <b-card-body>
+                  <b-row><p class="custom-titol">{{ asignatura }}</p></b-row>
+                  <b-row><p>Qualificació: 7.00</p></b-row>
+                </b-card-body>
+              </b-card>
+            </div>
+          </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
+
+<style scoped>
+@import '../assets/styles.css';
+@import '../assets/sd.css';
+</style>
 
 <script>
 import NavBar from '@/components/NavBar'
@@ -11,19 +65,10 @@ export default {
   },
   data () {
     return {
-      message: 'My first component',
-      tickets_bought: 0,
-      money_avaliable: 300
+      asignaturas: ['Lògica y Llenguatges', 'Factors Humans', 'Software Distribuït', 'Gràfics', 'Bases de Dades']
     }
   },
-  methods: {
-    buyTicket () {
-      this.tickets_bought += 1
-    },
-    returnTicket () {
-      this.tickets_bought -= 1
-    }
-  }
+  methods: {}
 }
 
 </script>
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,27 +1,21 @@
 <template>
   <div id="app">
     <b-navbar toggleable="lg" class="bluebg">
-
       <b-navbar-brand href="#">
         <img src="https://www.eim.ub.edu/img/ub-theme/images/logo_ub_negre.png" width="130px"
              class="d-inline-block align-top" alt="ub-logo">
       </b-navbar-brand>
-
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
       <b-collapse id="nav-collapse" is-nav>
-
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-
           <b-nav-item-dropdown size="lg" right variant="link" toggle-class="text-decoration-none" no-caret
                                style="margin-left: 5px">
             <template #button-content>
               <a style="color: #f4f8f8"> Nombre Apellido </a>
-              <b-avatar :src="avatarState" size="40px"></b-avatar>
+              <b-avatar size="40px"></b-avatar>
             </template>
             <b-dropdown-item>Perfil</b-dropdown-item>
-            <b-dropdown-item>Xat</b-dropdown-item>
             <b-dropdown-item>Canviar a Professor</b-dropdown-item>
             <b-dropdown-item>Tancar Sessió</b-dropdown-item>
           </b-nav-item-dropdown>
